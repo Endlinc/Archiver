99032dfccb06b23ca8b6dc08d6aff407e2df4f93
Endlinc <34116526+Endlinc@users.noreply.github.com>
2021-09-12 19:10:19 +0800

    add feature to compress list of files.

--- main/src/function/compressor.go
+++ main/src/function/compressor.go
@@ -2,42 +2,246 @@ package function
 
 import (
 	"archive/tar"
+	"compress/gzip"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"hash"
+	"io"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"sort"
+	"strings"
 )
 
-func Compress(dest string, paths []string) error {
-	fileWriter, fErr := os.Create(dest)
+type PathInfo struct {
+	path  string
+	isRec bool
+}
+
+type SortablePath []PathInfo
+
+func (a SortablePath) Len() int           { return len(a) }
+func (a SortablePath) Less(i, j int) bool { return a[i].path < a[j].path }
+func (a SortablePath) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+type Compressor struct {
+	incl    []PathInfo
+	excl    []PathInfo
+	relRoot string
+	fw      *os.File
+	gw      *gzip.Writer
+	tw      *tar.Writer
+	hasher  hash.Hash
+	n       int
+}
+
+func (c *Compressor) Init(dest string) error {
+	c.relRoot = string(filepath.Separator)
+	var err error
+	c.fw, err = os.Create(dest)
+	if err != nil {
+		return err
+	}
+	c.hasher = sha256.New()
+	mulWrt := io.MultiWriter(c.fw, c.hasher)
+	c.gw = gzip.NewWriter(mulWrt)
+	c.tw = tar.NewWriter(c.gw)
+	return nil
+}
+
+func (c *Compressor) Close() error {
+	fErr := c.fw.Close()
 	if fErr != nil {
 		fmt.Println(fErr.Error())
-		return fmt.Errorf("bad path for output archive: %s", dest)
 	}
-	defer func() {
-		if err := fileWriter.Close(); err != nil {
-			fmt.Println(err.Error())
-		}
-	}()
-
-	tarWriter := tar.NewWriter(fileWriter)
-	defer tarWriter.Close()
-
-	for _, path := range paths {
-		fi, pErr := os.Lstat(path)
-		if pErr != nil {
-			fmt.Println(pErr.Error())
-			return fmt.Errorf("%s is not valid file paht", path)
-		}
-
-		realPath, err := os.Readlink(path)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		hdr, err := tar.FileInfoHeader(fi, realPath)
-		err = tarWriter.WriteHeader(hdr)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
+	gErr := c.gw.Close()
+	if gErr != nil {
+		fmt.Println(gErr.Error())
 	}
-
+	tErr := c.tw.Close()
+	if tErr != nil {
+		fmt.Println(tErr.Error())
+	}
+	if fErr != nil || gErr != nil || tErr != nil {
+		return fmt.Errorf("failed during closing writer")
+	}
 	return nil
+}
+
+func (c *Compressor) FormSha256sum() string {
+	return hex.EncodeToString(c.hasher.Sum(nil))
+}
+
+func (c *Compressor) SetRelRoot(path string) {
+	c.relRoot = path
+}
+
+func (c *Compressor) LoadPaths(paths []string, isIncl bool) {
+	uniqPaths := unique(paths)
+	sort.Strings(uniqPaths)
+	uniqPaths = removeChildPath(uniqPaths)
+	if isIncl {
+		for _, path := range uniqPaths {
+			c.incl = append(c.incl, PathInfo{path: path, isRec: true})
+		}
+	} else {
+		for _, path := range uniqPaths {
+			c.excl = append(c.excl, PathInfo{path: path, isRec: true})
+		}
+	}
+}
+
+func (c *Compressor) AddAllPredecessors() {
+	predecessors := make([]string, 0, 10)
+	for _, pathInfo := range c.incl {
+		tp := pathInfo.path
+		prd := filepath.Dir(tp)
+		for !strings.EqualFold(prd, string(filepath.Separator)) {
+			predecessors = append(predecessors, prd)
+			prd = filepath.Dir(prd)
+		}
+	}
+	predecessors = unique(predecessors)
+	for _, predecessor := range predecessors {
+		c.incl = append(c.incl, PathInfo{path: predecessor, isRec: false})
+	}
+	sort.Sort(SortablePath(c.incl))
+}
+
+func (c *Compressor) Archive() error {
+	for _, inclPth := range c.incl {
+		if err := c.addContentToTar(inclPth); err != nil {
+			fmt.Printf("error occured during archive: %s", err.Error())
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *Compressor) addContentToTar(incl PathInfo) error {
+	if !incl.isRec {
+		info, err := os.Lstat(incl.path)
+		if err != nil {
+			return err
+		}
+		return c.writePathInfo(incl.path, info)
+	} else {
+		return filepath.Walk(incl.path, func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				fmt.Println(err.Error())
+				return nil
+			}
+			if err = c.writePathInfo(path, info); err != nil {
+				return err
+			}
+			if !info.Mode().IsRegular() {
+				return nil
+			}
+			return c.writeRegularFileContent(path)
+		})
+	}
+}
+
+func (c *Compressor) writePathInfo(path string, info fs.FileInfo) error {
+	if c.isOmittingPath(path, info) {
+		return nil
+	}
+	hdr, err := c.makeHeader(path, info)
+	if err != nil {
+		fmt.Println(err.Error())
+		return fmt.Errorf("cannot make header for %s", path)
+	}
+	if err := c.writeHeader(hdr); err != nil {
+		fmt.Println(err.Error())
+		return fmt.Errorf("cannot write header for %s", path)
+	}
+	return nil
+}
+
+func (c *Compressor) writeRegularFileContent(path string) error {
+	fr, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("cannot open file: %s", err.Error())
+		return fmt.Errorf("cannot open file %s", path)
+	}
+	_, err = io.Copy(c.tw, fr)
+	return err
+}
+
+func (c *Compressor) isOmittingPath(path string, info fs.FileInfo) bool {
+	if !(info.IsDir() || info.Mode().IsRegular() || info.Mode()&os.ModeSymlink != 0) {
+		return true
+	}
+	return c.isExcluded(path)
+}
+
+func (c *Compressor) isExcluded(path string) bool {
+	for c.n < len(c.excl) {
+		if strings.Compare(path, c.excl[c.n].path) < 0 {
+			return false
+		} else if strings.HasPrefix(path, c.excl[c.n].path) {
+			if isParDir(c.excl[c.n].path, path) {
+				return true
+			} else {
+				c.n++
+				continue
+			}
+		} else {
+			c.n++
+		}
+	}
+	return false
+}
+
+func (c *Compressor) makeHeader(path string, info fs.FileInfo) (hdr *tar.Header, err error) {
+	pointee := ""
+	if info.Mode()&os.ModeSymlink != 0 {
+		pointee, err = os.Readlink(path)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return tar.FileInfoHeader(info, pointee)
+}
+
+func (c *Compressor) writeHeader(hdr *tar.Header) error {
+	return c.tw.WriteHeader(hdr)
+}
+
+func unique(strSlice []string) []string {
+	keys := make(map[string]bool)
+	var list []string
+	for _, entry := range strSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
+func removeChildPath(paths []string) (parPaths []string) {
+	parPaths = make([]string, 0, 10)
+	parPaths = append(parPaths, paths...)
+	lastIdx := 0
+	for i := 1; i < len(parPaths); {
+		if strings.HasPrefix(parPaths[i], filepath.Join(parPaths[lastIdx], string(filepath.Separator))) {
+			parPaths = append(parPaths[:i], parPaths[i+1:]...)
+		} else {
+			lastIdx = i
+			i++
+		}
+	}
+	return parPaths
+}
+
+func isParDir(base, target string) bool {
+	relPath, err := filepath.Rel(base, target)
+	if err != nil {
+		fmt.Printf("cannot determine parent path: %s", err.Error())
+		return false
+	}
+	return strings.HasPrefix(relPath, "..")
 }
---
+++ main/src/function/compressor_test.go
@@ -0,0 +1,130 @@
+package function
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"fmt"
+	"hash"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestCompressor_LoadPaths(t *testing.T) {
+	type fields struct {
+		incl    []PathInfo
+		excl    []PathInfo
+		relRoot string
+		fw      *os.File
+		gw      *gzip.Writer
+		tw      *tar.Writer
+		hasher  hash.Hash
+	}
+	type args struct {
+		paths  []string
+		isIncl bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{name: "1", fields: fields{
+			incl:    nil,
+			excl:    nil,
+			relRoot: "",
+			fw:      nil,
+			gw:      nil,
+			tw:      nil,
+			hasher:  nil,
+		}, args: args{
+			paths:  []string{"/opt", "/proc", "/opt", "/opt/oss", "/home", "/usr"},
+			isIncl: false,
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Compressor{
+				incl:    tt.fields.incl,
+				excl:    tt.fields.excl,
+				relRoot: tt.fields.relRoot,
+				fw:      tt.fields.fw,
+				gw:      tt.fields.gw,
+				tw:      tt.fields.tw,
+				hasher:  tt.fields.hasher,
+			}
+			c.LoadPaths(tt.args.paths, tt.args.isIncl)
+			fmt.Println(c.excl)
+		})
+	}
+}
+
+func Test_removeChildPath(t *testing.T) {
+	type args struct {
+		paths []string
+	}
+	tests := []struct {
+		name         string
+		args         args
+		wantParPaths []string
+	}{
+		{name: "1", args: args{paths: []string{"/opt", "/opt/oss"}}, wantParPaths: []string{"/opt"}},
+		{name: "2", args: args{paths: []string{"/", "/opt", "/opt/oss"}}, wantParPaths: []string{"/"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotParPaths := removeChildPath(tt.args.paths); !reflect.DeepEqual(gotParPaths, tt.wantParPaths) {
+				t.Errorf("removeChildPath() = %v, want %v", gotParPaths, tt.wantParPaths)
+			}
+		})
+	}
+}
+
+func TestCompressor_addAllPredecessors(t *testing.T) {
+	type fields struct {
+		incl    []PathInfo
+		excl    []PathInfo
+		relRoot string
+		fw      *os.File
+		gw      *gzip.Writer
+		tw      *tar.Writer
+		hasher  hash.Hash
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{name: "1", fields: fields{
+			incl: []PathInfo{{
+				path:  "/opt/oss/manager",
+				isRec: false,
+			},
+				{
+					path:  "/backup",
+					isRec: false,
+				},
+			},
+			excl:    nil,
+			relRoot: "",
+			fw:      nil,
+			gw:      nil,
+			tw:      nil,
+			hasher:  nil,
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Compressor{
+				incl:    tt.fields.incl,
+				excl:    tt.fields.excl,
+				relRoot: tt.fields.relRoot,
+				fw:      tt.fields.fw,
+				gw:      tt.fields.gw,
+				tw:      tt.fields.tw,
+				hasher:  tt.fields.hasher,
+			}
+			c.AddAllPredecessors()
+			fmt.Println(c.incl)
+		})
+	}
+}
